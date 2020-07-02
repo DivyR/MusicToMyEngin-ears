d4c5768c3a71bcab296e62cfa5c602d3b0441c11
Geoffrey Jiang <geoffrey1jiang@gmail.com>
2020-07-02 13:17:22 -0400

    Create Waveform-Conv.py

---
+++ Waveform-Conv.py
@@ -0,0 +1,20 @@
+from scipy.io.wavfile import read
+from scipy import signal
+import matplotlib.pyplot as plt
+from pydub import AudioSegment
+import os
+
+arr = os.listdir("fma_small")
+reject = []
+for i in range(0,len(arr)):
+    arr2 = os.listdir("fma_small/"+arr[i])
+    for j in range(0,len(arr2)):
+        path = "fma_small/"+arr[i]+"/"+arr2[j]
+        if os.path.getsize(path) > 100000:
+            song = AudioSegment.from_mp3("fma_small/"+arr[i]+"/"+arr2[j])
+            song = song.set_channels(1)
+            name = os.path.splitext(arr2[j])[0]
+            song.export("newFMA/"+arr[i]+"/"+name+".wav", format="wav")
+        else:
+            reject += [arr2[j]]
+print(reject)
