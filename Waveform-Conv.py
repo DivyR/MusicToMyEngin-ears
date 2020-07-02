14bc1a3a54331a222a5b2b219b5dcbab9081ba02
Geoffrey Jiang <geoffrey1jiang@gmail.com>
2020-07-02 13:16:47 -0400

    Corrected code to convert all files
    Full code to convert the FMA subset to wav forms while removing corrupted files

--- Waveform-Conv.py
+++ Waveform-Conv.py
@@ -2,29 +2,19 @@ from scipy.io.wavfile import read
 from scipy import signal
 import matplotlib.pyplot as plt
 from pydub import AudioSegment
-input_data = read("file2.wav")
-audio = input_data[1]
-# plot the first 1024 samples
-plt.plot(audio[0:10000])
-# label the axes
-plt.ylabel("Amplitude")
-plt.xlabel("Time")
-# set the title
-plt.title("Sample Wav")
-# display the plot
-plt.show()
+import os
 
-#hi
-
-#song = AudioSegment.from_mp3("fma_small/000/000002.mp3")
-#song = song.set_channels(1)
-#song.export("file2.wav", format="wav")
-
-#sample_rate, samples = read('file.wav')
-#frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
-
-#plt.pcolormesh(times, frequencies, spectrogram)
-#plt.imshow(spectrogram)
-#plt.ylabel('Frequency [Hz]')
-#plt.xlabel('Time [sec]')
-#plt.show()
+arr = os.listdir("fma_small")
+reject = []
+for i in range(0,len(arr)):
+    arr2 = os.listdir("fma_small/"+arr[i])
+    for j in range(0,len(arr2)):
+        path = "fma_small/"+arr[i]+"/"+arr2[j]
+        if os.path.getsize(path) > 100000:
+            song = AudioSegment.from_mp3("fma_small/"+arr[i]+"/"+arr2[j])
+            song = song.set_channels(1)
+            name = os.path.splitext(arr2[j])[0]
+            song.export("newFMA/"+arr[i]+"/"+name+".wav", format="wav")
+        else:
+            reject += [arr2[j]]
+print(reject)
