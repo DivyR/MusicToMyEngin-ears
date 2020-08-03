21bc35c9bf862c2f13a9f53c89611402274362e8
Geoffrey Jiang <geoffrey1jiang@gmail.com>
2020-08-03 12:55:54 -0400

    Update WaveformGeneration.py

--- DataProcessing/WaveformGeneration.py
+++ DataProcessing/WaveformGeneration.py
@@ -1,17 +1,44 @@
+from scipy.io.wavfile import read
+from scipy import signal
+import matplotlib.pyplot as plt
 from pydub import AudioSegment
 import os
+import numpy as np
+import librosa
 
-arr = os.listdir("fma_small")
-reject = []
-for i in range(0,len(arr)):
-    arr2 = os.listdir("fma_small/"+arr[i])
-    for j in range(0,len(arr2)):
-        path = "fma_small/"+arr[i]+"/"+arr2[j]
-        if os.path.getsize(path) > 100000:
-            song = AudioSegment.from_mp3("fma_small/"+arr[i]+"/"+arr2[j])
-            song = song.set_channels(1)
+
+def split_file(path, filename, exportpath):
+    song = AudioSegment.from_wav(path+filename)
+    name = os.path.splitext(filename)[0]
+    genre = os.path.splitext(name)[0]
+    for i in range(0, 6):
+        split = song[i*5000:(i+1)*5000]
+        split.export(exportpath+name+"-"+str(i)+".wav", format="wav")
+
+
+
+def split_dataset():
+    arr = sorted(os.listdir("genres"))
+    for i in range(0,len(arr)):
+        arr2 = sorted(os.listdir("genres/"+arr[i]))
+        for j in range(0,len(arr2)):
+            path = "genres/"+arr[i]+"/"
+            filename = arr2[j]
+            exportpath = "NewDataSet/"+arr[i]+"/"
+            split(path,filename,exportpath)
+
+
+
+
+def mfcc(foldername,exportfolder):
+    arr = sorted(os.listdir(foldername))
+    for i in range(0,len(arr)):
+        arr2 = sorted(os.listdir(foldername+"/"+arr[i]))
+        for j in range(0,len(arr2)):
+            path = foldername+"/"+arr[i]+"/"+arr2[j]
+            y, s2 = librosa.load(path)
+            S = librosa.feature.melspectrogram(y, n_mels=128)
+            log_S = librosa.power_to_db(S, ref=np.max)
+            mfcc = librosa.feature.mfcc(S=log_S, n_mfcc=20)
             name = os.path.splitext(arr2[j])[0]
-            song.export("newFMA/"+arr[i]+"/"+name+".wav", format="wav")
-        else:
-            reject += [arr2[j]]
-print(reject)
+            np.savetxt(exportfolder+"/"+arr[i]+"/"+name+".txt", mfcc)
