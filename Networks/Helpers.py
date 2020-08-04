6eb2f63ee19e70f4f3df6d2c31d92d0195ffc159
DivyR <divy@yoga-c740.localdomain>
2020-08-03 20:20:21 -0400

    Push

---
+++ Networks/Helpers.py
@@ -0,0 +1,124 @@
+import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+torch.manual_seed(22)
+
+
+def get_model_name(name, batch_size, learning_rate, epoch):
+    """ Generate a name for the model consisting of all the hyperparameter values
+
+    Args:
+        config: Configuration object containing the hyperparameters
+    Returns:
+        path: A string with the hyperparameter name and value concatenated
+    """
+    path = "model_{0}_bs{1}_lr{2}_epoch{3}".format(
+        name, batch_size, learning_rate, epoch
+    )
+    return path
+
+
+def plot_training_curve(path):
+    """ Plots the training curve for a model run, given the csv files
+    containing the train/validation error/loss.
+
+    Args:
+        path: The base path of the csv files produced during training
+    """
+    import matplotlib.pyplot as plt
+
+    train_acc = np.loadtxt("{}_train_acc.csv".format(path))
+    val_acc = np.loadtxt("{}_val_acc.csv".format(path))
+    plt.title("Train and Validation Accuracy")
+    n = len(train_acc)  # number of epochs
+    plt.plot(range(1, n + 1), train_acc, label="Train")
+    plt.plot(range(1, n + 1), val_acc, label="Validation")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.legend(loc="best")
+    plt.show()
+
+    train_loss = np.loadtxt("{}_train_loss.csv".format(path))
+    val_loss = np.loadtxt("{}_val_loss.csv".format(path))
+    plt.title("Train vs Validation Loss")
+    plt.plot(range(1, n + 1), train_loss, label="Train")
+    plt.plot(range(1, n + 1), val_loss, label="Validation")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.legend(loc="best")
+    plt.show()
+
+
+def get_accuracy(model, data, criterion, batch_size=32):
+    """ Compute the accuracy of the `model` across a dataset `data`
+    I MODIFIED THIS FUNCTION TO ALSO RETURN THE LOSS
+    """
+    correct, total = 0, 0
+    loss = 0.0
+    for i, batch in enumerate(data):
+        labels, mfccs = batch
+        output = model(mfccs)
+
+        loss += criterion(output, labels).item()
+
+        pred = output.max(1, keepdim=True)[1]
+        correct += pred.eq(batch.labels.view_as(pred)).sum().item()
+        total += labels.shape[0]
+    return correct / total, loss / (i + 1)
+
+
+def train(model, train, valid, learning_rate, batch_size, num_epochs=30):
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    # accuracy tracking
+    train_acc, valid_acc = [], []
+    train_loss, valid_loss = [], []
+
+    # training network
+    print("Training Started...")
+    startTime = time.time()
+
+    for epoch in range(num_epochs):
+        for labels, mfccs in train:
+            optimizer.zero_grad()
+
+            output = model(mfccs.float())
+            loss = criterion(output, labels)
+            loss.backward()
+            optimizer.step()
+
+        tacc, tloss = get_accuracy(model, train, criterion, batch_size=batch_size)
+        vacc, vloss = get_accuracy(model, valid, criterion, batch_size=batch_size)
+
+        train_acc.append(tacc)
+        valid_acc.append(vacc)
+        train_loss.append(tloss)
+        valid_loss.append(vloss)
+
+        print(
+            (
+                "Epoch {}: Train acc: {}, Train loss: {} | "
+                + "Validation acc: {}, Validation loss: {}"
+            ).format(
+                epoch + 1, train_acc[-1], train_loss[-1], valid_acc[-1], valid_loss[-1]
+            )
+        )
+
+        modelPath = get_model_name(model.name, batch_size, learning_rate, epoch + 1)
+        np.savetxt("{}_train_acc.csv".format(modelPath), train_acc)
+        np.savetxt("{}_val_acc.csv".format(modelPath), valid_acc)
+        np.savetxt("{}_train_loss.csv".format(modelPath), train_loss)
+        np.savetxt("{}_val_loss.csv".format(modelPath), valid_loss)
+
+        if valid_loss[-1] == min(valid_loss) or valid_acc[-1] == max(
+            valid_acc
+        ):  # only save state in this case!!
+            torch.save(model.state_dict(), modelPath)
+
+    endTime = time.time()
+    elapsedTime = endTime - startTime
+    print("Total time elapsed: {:.2f} seconds".format(elapsedTime))
+    print("Finished Training")
--- Networks/crnn.py
+++ Networks/crnn.py
@@ -8,10 +8,10 @@ class CRNN(nn.Module):
         super(CRNN, self).__init__()
         self.name = name
         self.conv1 = nn.Conv2d(1, 3, 5)  # channels in, channels out, kernel
-        self.conv2 = nn.Conv2d(3, 6, 6)
+        self.conv2 = nn.Conv2d(3, 6, 8)
         self.avgpool = nn.AvgPool2d(2, 2)  # kernal size, stride
         self.rnn = nn.GRU(
-            input_size=100,
+            input_size=6,
             hidden_size=hiddenSize,
             num_layers=numLayers,
             batch_first=True,
@@ -22,14 +22,14 @@ class CRNN(nn.Module):
         # CONVOLUTION
         x = x.unsqueeze(1)  # [batch, 20, 213]  -> [batch, 1, 20, 213]
         x = F.relu(self.conv1(x))  # [batch, 1, 20, 213] -> [batch, 3, 16, 209]
-        x = self.avgpool(x)  # [batch, 3, 16, 209] -> [batch, 6, 8, 105]
-        x = F.relu(self.conv2(x))  # [batch, 6, 105] -> [batch, 6, 1, 100]
+        x = self.avgpool(x)  # [batch, 3, 16, 209] -> [batch, 3, 8, 104]
+        x = F.relu(self.conv2(x))  # [batch, 3, 8, 104] -> [batch, 6, 1, 97]
 
         # RNN
-        x = x.squeeze(2)  # [batch, 6, 1, 100] -> [batch, 6, 100]
+        x = x.squeeze(2)  # [batch, 6, 1, 97] -> [batch, 6, 97]
         # each channel is a feature where the x-axis represents time,
         # so we want sequences in the x-direction each with 1 feature from each channel
-        x = x.transpose(1, 2)  # [batch, 100, 6]
+        x = x.transpose(1, 2)  # [batch, 97, 6]
         out, _ = self.rnn(x)
 
         # CLASSIFIER
